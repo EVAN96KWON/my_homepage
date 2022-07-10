6253b7d8f836ab5f1f877f09883a885af3046fd0
heonjin <koun012@naver.com>
2022-07-10 15:54:28 +0900

    this may be useless

--- lib/utils/Tile.dart
+++ lib/utils/Tile.dart
@@ -24,31 +24,45 @@ class Tile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      child: Card(
-        elevation: 15,
-        clipBehavior: Clip.antiAlias,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Column(
-          children: [
-            ListTile(
-              leading: Image(
-                image: AssetImage(getImageName(repos)),
-                fit: BoxFit.fitHeight,
+    return GestureDetector(
+      onTap: () {launchUrl(Uri.parse(repos.htmlUrl!));},
+      child: Container(
+        height: 200,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        child: Card(
+          elevation: 15,
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            children: [
+              ListTile(
+                leading: Image(
+                  image: AssetImage(getImageName(repos)),
+                  fit: BoxFit.fitHeight,
+                ),
+                title: Text(ReCase(repos.name!).titleCase,
+                    style: TextStyle(fontSize: 32)),
+                subtitle: Text(
+                  repos.description!,
+                  style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                ),
               ),
-              title: Text(ReCase(repos.name!).titleCase,
-                  style: TextStyle(fontSize: 32)),
-              subtitle: Text(
-                repos.description!,
-                style: TextStyle(color: Colors.black.withOpacity(0.6)),
-              ),
-            ),
-          ],
+              Row(
+                children: [
+                  ...repos.topics!.map((e) => Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 7),
+                    child: ActionChip(
+                      label: Text(e),
+                      onPressed: () {},
+                    ),
+                  )),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
